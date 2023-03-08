7fd4679bee1404578ca088663b381f6938ece17a
Charles GOEDEFROIT <charles.goedefroit@inria.fr>
2023-03-08 15:47:47 +0100

    update types

--- sample/types.h
+++ sample/types.h
@@ -1,14 +1,40 @@
 #ifndef CUSTOM_TYPES_H
 #define CUSTOM_TYPES_H 1
 
-typedef signed char int8;
-typedef signed short int16;
-typedef signed int int32;
-typedef signed long int64;
+#define PORTABLE
 
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long uint64;
+#ifdef PORTABLE
+#include <linux/types.h>
+
+typedef __s8 s8;
+typedef __s16 s16;
+typedef __s32 s32;
+typedef __s64 s64;
+
+typedef __u8 u8;
+typedef __u16 u16;
+typedef __u32 u32;
+typedef __u64 u64;
+#else
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+#endif /* PORTABLE */
+
+typedef s8 int8;
+typedef s16 int16;
+typedef s32 int32;
+typedef s64 int64;
+
+typedef u8 uint8;
+typedef u16 uint16;
+typedef u32 uint32;
+typedef u64 uint64;
 
 #endif /* CUSTOM_TYPES_H */
